e8ee84155c5564a5a09313df7b772b2995321a9c
Luis Rey Lara <luisreylara@hotmail.com>
2025-06-09 06:01:38 -0600

    Add files via upload

---
+++ html/IntroCss.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+    <h1>Contenido</h1>
+    <p>este es el nuevo párrafo</p>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+    h1{
+        color:blue; 
+    }
+    p{
+        color:white;
+        background-color: crimson;
+    }
+</style>
