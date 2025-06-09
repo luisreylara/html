2a7b047c0cc8dca3d177efef5cfbcd83ec86597d
Luis Rey Lara <luisreylara@hotmail.com>
2025-06-09 06:14:41 -0600

    Add files via upload

---
+++ html/IntroTablas.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <table border="1">
+        <tr>
+            <th>Nombre</th>
+            <th>Apellido</th>
+            <th>Email</th>
+        </tr>
+        <tr>
+            <td>Juan</td>
+            <td>Perez</td>
+            <td>jperez@mail.com</td>
+        </tr>
+        <tr>
+            <td>María</td>
+            <td>Lara</td>
+            <td>mlara@mail.com</td>
+        </tr>
+    </table>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
