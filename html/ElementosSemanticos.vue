ab17d698fc9838dd849e8c72874876ad44ef80ac
Luis Rey Lara <luisreylara@hotmail.com>
2025-06-11 18:23:44 -0600

    Create ElementosSemanticos.vue

---
+++ html/ElementosSemanticos.vue
@@ -0,0 +1,42 @@
+<template>
+     <header>
+        <h1>Etiquetas Semánticas en HTML5</h1>
+    </header>
+    <nav>
+        <a href="#">Inicio</a>
+        <a href="#">Servicios</a>
+        <a href="#">Contacto</a>
+        <a href="#">Acerca De</a>
+    </nav>
+    <section>
+        <h2>Sección 1</h2>
+        <p>Información de la sección.</p>
+        <aside>
+            <p>Nombre del Autor</p>
+        </aside>
+    </section>
+    <section>
+        <h2>Sección 2</h2>
+        <article>
+            <h3>Artículo 1</h3>
+        </article>
+        <article>
+            <h3>Artículo 2</h3>
+        </article>
+    </section>
+    <footer>
+        <h2>
+            <a href="http://www.globalmentoring.com.mx">
+                GlobalMentoring.com.mx
+            </a>
+        </h2>
+    </footer>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
